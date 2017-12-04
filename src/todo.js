0026ad4556a70f9174db3f78db6e1979eec53e12
Chris Gonzales <cm.gonzales@yahoo.com>
2017-12-03 18:44:59 -0800

    added onChange

--- src/todo.js
+++ src/todo.js
@@ -1,20 +1,36 @@
 import React from 'react';
 
 
-class todo extends React.Component{
+ 
+
+class Todo extends React.Component{
+// state is the father props is the child
 
     constructor(props){
         super(props)
         
-        }
-      
+        this.state ={
+           input: "" 
+        };
+        
+        this.charge = this.charge.bind(this);
+
+    }
+
+         charge(e) {
+         var result = e.target.value;
+         this.setState({input: result}); 
+
+         }
 
     render(){
         return(
             <div>
-            <li>{props.state.Todo}</li>
+            <li> {this.props.Todo}</li>
+            <input value ={this.state.input} onChange={this.charge}/>
+            <p>{this.state.input}</p>
             </div>
         )
     }
 }
-export default todo;
+export default Todo;
